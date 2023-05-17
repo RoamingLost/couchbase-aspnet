e823c258bc6080225c2ad60269febd46a03b456b
John Stanley <jstanley@connecture.com>
2023-05-17 14:14:49 -0700

    add configuration and return object definitions from 2.x under AspNet namespace

---
+++ src/Couchbase.AspNet/Configuration/Client/ClientConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Couchbase.AspNet.Authentication;
+
+namespace Couchbase.AspNet.Configuration.Client
+{
+    public static class ClientConfigurationExtensions
+    {
+        public static ClusterOptions ToClusterOptions(this ClientConfiguration config)
+        {
+            var passwordAuthenticator = config.Authenticator as PasswordAuthenticator;
+            return new ClusterOptions
+            {
+                ConnectionString = string.Join(",", config.Servers.Select(x => x.ToString())),
+                UserName = passwordAuthenticator?.Username,
+                Password = passwordAuthenticator?.Password,
+                Buckets = config.BucketConfigs.Keys.ToList()
+            };
+        }
+    }
+}
